97683bfc5a86c03c0adb4d29dcd455b8dfbbe9ba
= <=>
2022-10-12 12:53:46 +0200

    some git keybinds

--- lua/albin/remap.lua
+++ lua/albin/remap.lua
@@ -1,3 +1,7 @@
 local nnoremap = require("albin.keymap").nnoremap
 
 nnoremap("<leader>e", "<cmd>Ex<CR>")
+nnoremap("<leader>gd", "<cmd>Git diff<CR>")
+nnoremap("<leader>gs", "<cmd>Git status<CR>")
+nnoremap("<leader>ga", ":Git add ")
+nnoremap("<leader>gc", ":Git commit -m ''")
